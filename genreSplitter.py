91a97aceae1d8809b5e41d65241d653e1bcdd734
Derek Wallace <17050474+PresidentSillyHat@users.noreply.github.com>
2017-12-04 11:07:42 -0700

    comments

--- genreSplitter.py
+++ genreSplitter.py
@@ -1,14 +1,14 @@
 import csv
 
-class GenreS: 
+class GenreS: #this is for breaking genres down into single genres for use with classification
     readfrom=""
 
-    def __init__(self,toRead):
+    def __init__(self,toRead):  
         readfrom=toRead
 
-    def splitter(self):
+    def splitter(self): #the actual method to split
         mark=0
-        toWrite="q2fix.csv"
+        toWrite="q2fix.csv" #file results are saved to
         with open(self.readfrom, 'r') as file:
             with open(toWrite, 'w', newline='') as csvfile:
                 csv_reader = csv.reader(file, delimiter=",")
@@ -24,16 +24,16 @@ class GenreS:
                     #if "|" in str(row)
                     if "|" in row[0]:
                         i=row[0].index("|")
-                        genre=row[0][0:i]
+                        genre=row[0][0:i]   #edit one column of the row
                         row2=row
                         row2[0]=genre
                         #print("reached")
                         #print(genre)
-                        writer.writerow(row2)
+                        writer.writerow(row2) #write the editted row into the toWrite file
                         
                     else:
                         #print(row[0])
-                        writer.writerow(row)
+                        writer.writerow(row)    #if only one genre in genres, no need to fix
         return toWrite
                     
                 
